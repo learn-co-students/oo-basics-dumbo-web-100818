bd2a859df9974c1622b97506b0a907df44d557fa
achadha2 <andy.chadha2@gmail.com>
2018-10-10 12:25:17 -0400

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,18 @@
+require 'pry'
+class Book
+  def initialize(title="", page_count="", genre="")
+    @title = title
+    @page_count = page_count
+    @genre = genre
+  end
+
+  attr_accessor :title, :author, :page_count, :genre
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
 
 
+end
+
+moby = Book.new
